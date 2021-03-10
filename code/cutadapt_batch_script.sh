7a937fac9cc796473c1895f9f17eb52d8fa0a19d
Matomo Niwano <47988393+matomoniwano@users.noreply.github.com>
2021-03-10 11:19:37 +0100

    Update cutadapt_batch_script.sh
    Added annotations and descriptions

--- code/cutadapt_batch_script.sh
+++ code/cutadapt_batch_script.sh
@@ -4,10 +4,13 @@
 # $2 = forward primers
 # $3 = reverse primers
 
+# Changing input field separators to new line (\n)
 IFS=$'\n'
 
+# Create a variable for the current path
 current=$PWD
 
+# Error message for missing positional arguments when running this script
 if [ $# -ne 3 ] ; then
     echo "Usage:"
     echo "$0 takes three parameters, 
@@ -19,35 +22,48 @@ else
     echo 'Running cutadapt in batches'
 fi
 
+# Adding a path to fastq directory from the positional argument $1 
 fastqpath=$1
+# Adding a fwd primers sequence from the positional argument $2
 fwdprm=$2
+# Adding a rvs primers sequence from the positional argument $3
 rvsprm=$3
 
+# Extracting fwd fastq file names
 fwd=($(ls -1v $1 | grep 'R1'))
 
+# Creating a new directory for clipped fastq files
 mkdir $1/processed_files 
 
+# Move to fastq directory
 cd $1
 
+# Running cutadapt using forloop for each pair-end fastq files. 
 for rvsfastq in "${fwd[@]}"
 do
 	rvsname=$(echo "$rvsfastq" | sed -e "s/R1/R2/")
-	cutadapt -g "$fwdprm" -G "$rvsprm" --discard-untrimmed -o ./processed_files/"$rvsfastq" -p ./processed_files/"$rvsname" "$rvsfastq" "$rvsname" 
+	echo "$rvsfastq" >> ~/CTD_analysis/code/cutadapt_summary.txt
+	cutadapt -g "$fwdprm" -G "$rvsprm" --discard-untrimmed -o ./processed_files/"$rvsfastq" -p ./processed_files/"$rvsname" "$rvsfastq" "$rvsname" >> ~/CTD_analysis/code/cutadapt_summary.txt
 	echo "$rvsfastq" >> ~/CTD_analysis/code/seqnames.txt
 done
 
+# Counting sequence reads from raw fastq files
 while read line
 do
 ls -1v "${line}" | xargs wc -l | grep -v "total" | awk '{print $1/4}' >> ~/CTD_analysis/code/seqreads_raw.txt
 done < ~/CTD_analysis/code/seqnames.txt
 
+# Move to processed fastq directory
 cd $1/processed_files
 
+# Counting sequence reads from clipped fastq files
 while read line
 do
 ls -1v "${line}" | xargs wc -l | grep -v "total" | awk '{print $1/4}' >> ~/CTD_analysis/code/seqreads_clipped.txt
 done < ~/CTD_analysis/code/seqnames.txt
 
+# Back to the working directory
 cd $current
 
+# Create a table with all the stats
 paste seqnames.txt seqreads_raw.txt seqreads_clipped.txt > seqtable.txt
